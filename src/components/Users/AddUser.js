7a2a0a270fe09f6a3913260ac7e1a2ed0af26496
Vetal Turkevich <vetalturkevich@gmail.com>
2021-07-16 07:57:59 -0400

    Portals

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -46,7 +46,7 @@ function AddUser(props) {
     }
 
     return (
-        <React.Fragment>
+        <>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/> }
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -57,7 +57,7 @@ function AddUser(props) {
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
-        </React.Fragment>
+        </>
     )
 }
 
